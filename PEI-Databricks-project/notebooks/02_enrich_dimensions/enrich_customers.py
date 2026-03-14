891c9d374dc8ba6d6159a0571f648c9c792b9ca8
Naankarthik <s.karthikeyan1100@gmail.com>
2026-03-14 15:09:08 +0000

    remove special character

--- PEI-Databricks-project/notebooks/02_enrich_dimensions/enrich_customers.py
+++ PEI-Databricks-project/notebooks/02_enrich_dimensions/enrich_customers.py
@@ -1,18 +1,17 @@
 # load raw table
 
-from pyspark.sql.functions import col
+from pyspark.sql.functions import col, regexp_replace
 from delta.tables import DeltaTable
 raw_customers = spark.table("raw_customers")
 
 enriched_customers = raw_customers.select(
     col("customer_id"),
-    col("customer_name"),
+    regexp_replace(col("customer_name"), r"[^a-zA-Z0-9\s]", "").alias("customer_name"),
     col("segment"),
     col("country"),
     col("city"),
     col("state")
 )
-
 if spark.catalog.tableExists("enriched_customers"):
     delta_table = DeltaTable.forName(spark, "enriched_customers")
     delta_table.alias("target").merge(
